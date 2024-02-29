67b5f73557352f08a46fefb2c04ebcd93a5c0470
prameela <prameelarpujari>
2024-02-29 12:14:26 +0530

    Completed first project that is expanding-cards.

---
+++ expanding-cards/code/index.js
@@ -0,0 +1,18 @@
+// connect bridge to html 
+const images=document.querySelectorAll('.image');
+
+images.forEach(image =>{
+    image.addEventListener('click', ()=>{
+        removeActiveClass();
+        image.classList.add('active');
+    });
+});
+
+const removeActiveClass=( )=>{
+    images.forEach(image =>{
+     image.classList.remove('active');
+    });
+};
+
+
+
